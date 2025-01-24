789d924870ebe9512fa463b161ce33b2c737a116
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2025-01-24 15:57:30 -0300

    updated edit profile page

--- resources/js/pages/Profile/Edit.tsx
+++ resources/js/pages/Profile/Edit.tsx
@@ -10,32 +10,23 @@ export default function Edit({
     status,
 }: PageProps<{ mustVerifyEmail: boolean; status?: string }>) {
     return (
-        <AuthenticatedLayout
-            header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                    Profile
-                </h2>
-            }
-        >
+        <AuthenticatedLayout>
             <Head title="Profile" />
 
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
-                    <div className="bg-white p-4 shadow dark:bg-gray-800 sm:rounded-lg sm:p-8">
-                        <UpdateProfileInformationForm
-                            mustVerifyEmail={mustVerifyEmail}
-                            status={status}
-                            className="max-w-xl"
-                        />
-                    </div>
+            <div className="flex max-w-7xl flex-col sm:px-6 lg:px-8">
+                <div className="p-4 shadow sm:p-8">
+                    <UpdateProfileInformationForm
+                        mustVerifyEmail={mustVerifyEmail}
+                        status={status}
+                    />
+                </div>
 
-                    <div className="bg-white p-4 shadow dark:bg-gray-800 sm:rounded-lg sm:p-8">
-                        <UpdatePasswordForm className="max-w-xl" />
-                    </div>
+                <div className="p-4 shadow sm:p-8">
+                    <UpdatePasswordForm />
+                </div>
 
-                    <div className="bg-white p-4 shadow dark:bg-gray-800 sm:rounded-lg sm:p-8">
-                        <DeleteUserForm className="max-w-xl" />
-                    </div>
+                <div className="p-4 shadow sm:p-8">
+                    <DeleteUserForm />
                 </div>
             </div>
         </AuthenticatedLayout>
