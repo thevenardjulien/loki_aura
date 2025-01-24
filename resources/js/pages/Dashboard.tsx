fcb42fd82fd4cf04bf7e18b6a080b6eeeb8c7071
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2025-01-24 16:30:00 -0300

    updated dashboard

--- resources/js/pages/Dashboard.tsx
+++ resources/js/pages/Dashboard.tsx
@@ -6,7 +6,14 @@ export default function Dashboard() {
         <AuthenticatedLayout>
             <Head title="Dashboard" />
 
-            <h1>You're logged in!</h1>
+            <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                    <div className="aspect-video rounded-xl bg-sidebar" />
+                    <div className="aspect-video rounded-xl bg-sidebar" />
+                    <div className="aspect-video rounded-xl bg-sidebar" />
+                </div>
+                <div className="min-h-[100vh] flex-1 rounded-xl bg-sidebar md:min-h-min" />
+            </div>
         </AuthenticatedLayout>
     );
 }
