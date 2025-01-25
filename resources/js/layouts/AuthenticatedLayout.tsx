590deec9418e6b89fc55a6eed802a19c328ca8f2
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2025-01-25 11:06:13 -0300

    removed command from authenticated layout

--- resources/js/layouts/AuthenticatedLayout.tsx
+++ resources/js/layouts/AuthenticatedLayout.tsx
@@ -1,30 +1,12 @@
 import { AppBreadcrumb } from '@/components/app-breadcrumb';
 import { AppSidebar } from '@/components/app-sidebar';
 import { ThemeProvider } from '@/components/theme-provider';
-import {
-    CommandDialog,
-    CommandEmpty,
-    CommandGroup,
-    CommandInput,
-    CommandItem,
-    CommandList,
-    CommandSeparator,
-    CommandShortcut,
-} from '@/components/ui/command';
 import { Separator } from '@/components/ui/separator';
 import {
     SidebarInset,
     SidebarProvider,
     SidebarTrigger,
 } from '@/components/ui/sidebar';
-import {
-    Calculator,
-    Calendar,
-    CreditCard,
-    Settings,
-    Smile,
-    User,
-} from 'lucide-react';
 import { useEffect, useState } from 'react';
 
 export default function AuthenticatedLayout({
@@ -65,44 +47,6 @@ export default function AuthenticatedLayout({
                         {children}
                     </div>
                 </SidebarInset>
-                <CommandDialog open={open} onOpenChange={setOpen}>
-                    <CommandInput placeholder="Type a command or search..." />
-                    <CommandList>
-                        <CommandEmpty>No results found.</CommandEmpty>
-                        <CommandGroup heading="Suggestions">
-                            <CommandItem>
-                                <Calendar />
-                                <span>Calendar</span>
-                            </CommandItem>
-                            <CommandItem>
-                                <Smile />
-                                <span>Search Emoji</span>
-                            </CommandItem>
-                            <CommandItem>
-                                <Calculator />
-                                <span>Calculator</span>
-                            </CommandItem>
-                        </CommandGroup>
-                        <CommandSeparator />
-                        <CommandGroup heading="Settings">
-                            <CommandItem>
-                                <User />
-                                <span>Profile</span>
-                                <CommandShortcut>⌘P</CommandShortcut>
-                            </CommandItem>
-                            <CommandItem>
-                                <CreditCard />
-                                <span>Billing</span>
-                                <CommandShortcut>⌘B</CommandShortcut>
-                            </CommandItem>
-                            <CommandItem>
-                                <Settings />
-                                <span>Settings</span>
-                                <CommandShortcut>⌘S</CommandShortcut>
-                            </CommandItem>
-                        </CommandGroup>
-                    </CommandList>
-                </CommandDialog>
             </SidebarProvider>
         </ThemeProvider>
     );
