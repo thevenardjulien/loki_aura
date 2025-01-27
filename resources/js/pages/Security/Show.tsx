0efc5d88aff6f654aed89d946529886114463437
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2025-01-27 14:23:39 -0300

    renamed account profile page and added account security page

---
+++ resources/js/pages/Security/Show.tsx
@@ -0,0 +1,34 @@
+import AuthenticatedLayout from '@/layouts/AuthenticatedLayout';
+import { PageProps } from '@/types';
+import { Head } from '@inertiajs/react';
+import DeleteUserForm from './Partials/DeleteUserForm';
+import UpdatePasswordForm from './Partials/UpdatePasswordForm';
+import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
+
+export default function Edit({
+    mustVerifyEmail,
+    status,
+}: PageProps<{ mustVerifyEmail: boolean; status?: string }>) {
+    return (
+        <AuthenticatedLayout>
+            <Head title="Profile" />
+
+            <div className="flex max-w-7xl flex-col sm:px-6 lg:px-8">
+                <div className="p-4 sm:p-8">
+                    <UpdateProfileInformationForm
+                        mustVerifyEmail={mustVerifyEmail}
+                        status={status}
+                    />
+                </div>
+
+                <div className="p-4 sm:p-8">
+                    <UpdatePasswordForm />
+                </div>
+
+                <div className="p-4 sm:p-8">
+                    <DeleteUserForm />
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
