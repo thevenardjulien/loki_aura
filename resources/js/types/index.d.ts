fc0107c721a77f3fb9854fe5c47e057c5567280c
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2025-01-28 19:32:26 -0300

    added more Session model, migration, types and route

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -6,6 +6,34 @@ export interface User {
     email: string;
     email_verified_at?: string;
     profile_photo_url: string | undefined;
+    two_factor_enabled: boolean;
+}
+
+interface Session {
+    id: string;
+    agent: {
+        platform: string;
+        browser: string;
+        is_desktop: boolean;
+    };
+    ip_address: string;
+    real_ip: string;
+    is_current_device: boolean;
+    last_activity: string;
+    last_active_ago: string;
+    browser: string;
+    browser_version: string;
+    os: string;
+    os_version: string;
+    device_type: string;
+    location: string | null;
+    device_details: {
+        is_mobile: boolean;
+        is_tablet: boolean;
+        is_desktop: boolean;
+        device: string;
+        languages: string[];
+    };
 }
 
 export type PageProps<
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\AccountProfileController;
-use App\Http\Controllers\AccountSecurityController;
+use App\Http\Controllers\Account\ProfileController;
+use App\Http\Controllers\Account\SecurityController;
+use App\Http\Controllers\Account\SessionController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -20,11 +21,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/account/profile', [AccountProfileController::class, 'show'])->name('profile.show');
-    Route::patch('/account/profile', [AccountProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/account/profile', [AccountProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/account/profile', [ProfileController::class, 'show'])->name('profile.show');
+    Route::patch('/account/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/account/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/account/security', [AccountSecurityController::class, 'show'])->name('security.show');
+    Route::get('/account/security', [SecurityController::class, 'show'])->name('security.show');
+
+    Route::delete('/account/sessions/other-browser-sessions', [SessionController::class, 'destroyOtherSessions'])
+        ->name('other-browser-sessions.destroy');
+    Route::delete('/account/sessions/browser-sessions/{id}', [SessionController::class, 'destroySession'])
+        ->name('browser-sessions.destroy');
 
     // Projects routes
     Route::get('/projects', function () {
