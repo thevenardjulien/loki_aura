1c7271abc4ebbde004d63110c3a3db48c9e2911e
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2025-02-01 15:01:29 -0300

    feat: added a nicer welcome page

--- resources/js/pages/Welcome.tsx
+++ resources/js/pages/Welcome.tsx
@@ -1,3 +1,4 @@
+import { Ripple } from '@/components/ui/ripple';
 import { PageProps } from '@/types';
 import { Head, Link } from '@inertiajs/react';
 import { Command } from 'lucide-react';
@@ -26,7 +27,7 @@ export default function Welcome({ auth }: PageProps) {
                             {auth.user ? (
                                 <Link
                                     href={route('dashboard')}
-                                    className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    className="rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                 >
                                     Dashboard
                                 </Link>
@@ -34,13 +35,13 @@ export default function Welcome({ auth }: PageProps) {
                                 <>
                                     <Link
                                         href={route('login')}
-                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        className="rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
                                         Log in
                                     </Link>
                                     <Link
                                         href={route('register')}
-                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        className="rounded-md text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
                                         Register
                                     </Link>
@@ -51,11 +52,12 @@ export default function Welcome({ auth }: PageProps) {
                 </header>
 
                 {/* Main Content */}
-                <main className="mx-auto h-full w-full max-w-7xl grow p-6">
-                    <div className="flex h-full items-center justify-center rounded-lg border-2 border-dashed">
-                        <p className="text-lg text-muted-foreground">
+                <main className="mx-auto h-full w-full max-w-7xl grow">
+                    <div className="relative flex h-full items-center justify-center">
+                        <p className="text-[clamp(1.5rem,5vw,4rem)] leading-none">
                             Your landing goes here
                         </p>
+                        <Ripple />
                     </div>
                 </main>
             </div>
